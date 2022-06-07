e737c744059073c147131af4e3c226df95109fba
itouyoshiki <k021g1467@g.neec.ac.jp>
2022-06-07 12:23:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Assets/Shot.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    // ゲームオブジェクトをインスペクターから参照するための変数
+    public GameObject Bullet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // 弾を生成
+            Instantiate(Bullet, transform.position, Quaternion.identity);
+        }
+    }
+}
